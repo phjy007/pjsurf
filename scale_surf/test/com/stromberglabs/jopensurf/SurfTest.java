b296079030d7d7b80e1c055cba3830828ca6d4a1
Arthur Yoo <phjy007@gmail.com>
2012-06-12 12:05:01 +0800

    initial scale_parallelism

---
+++ scale_surf/test/com/stromberglabs/jopensurf/SurfTest.java
@@ -0,0 +1,16 @@
+package com.stromberglabs.jopensurf;
+
+import static org.junit.Assert.assertTrue;
+
+import java.io.File;
+
+import javax.imageio.ImageIO;
+
+import org.junit.Test;
+
+
+public class SurfTest {
+	@Test
+	public void testSURFOutput() throws Exception {
+	}
+}
