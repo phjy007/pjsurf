c6e4615b1bc435f5af7a9f1a46b8c75e40dc9ff1
Arthur Yoo <phjy007@gmail.com>
2012-06-12 12:14:57 +0800

    delete

--- scale_surf/src/com/stromberglabs/jopensurf/InterestPoint.java
+++
@@ -1,7 +0,0 @@
-package com.stromberglabs.jopensurf;
-
-import com.stromberglabs.cluster.Clusterable;
-
-public interface InterestPoint extends Clusterable {
-	public double getDistance(InterestPoint point);
-}
